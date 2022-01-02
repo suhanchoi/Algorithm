79c11a7055cbe3955956c9012f04fef2353ab79d
suhan <suhan9603@naver.com>
2022-01-02 19:02:51 +0900

    0102 update

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import sys
-# num = int(sys.stdin.readline())
+num1 = int(sys.stdin.readline())
+num2 = int(sys.stdin.readline())
 
-def chk(n):
-    cnt = 0
-    while n//10 > 0:
-        cnt += 1
-        n /= 10
-    return cnt
+# def chk(n):
+#     cnt = 0
+#     while n//10 > 0:
+#         cnt += 1
+#         n /= 10
+#     return cnt
 
-print(chk(552))
+print(102)
+print('suhan0319')
