34a414377df8f56c7ffb8e203a4a1fb0e8d7853c
suhan <suhan9603@naver.com>
2022-01-03 21:23:11 +0900

    0103 update

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import sys
-num1 = int(sys.stdin.readline())
-num2 = int(sys.stdin.readline())
+num1, num2 = map(int,(sys.stdin.readline().split()))
+
+# num2 = int(sys.stdin.readline())
+
+print(num2*2 - num1)
 
 # def chk(n):
 #     cnt = 0
 #     while n//10 > 0:
 #         cnt += 1
 #         n /= 10
-#     return cnt
 
+#     return cnt
