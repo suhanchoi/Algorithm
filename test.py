2f9eeb6f275e2a55b82773d4271e549124bc66a3
suhan <suhan9603@naver.com>
2022-01-17 09:20:52 +0900

    0117 update

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
-import sys
+# import sys
 # a,b = map(int,(sys.stdin.readline().split()))
-a,b = sys.stdin.readline().split()
-print(eval(a+'+'+b))
+# a,b = sys.stdin.readline().split()
+# print(eval(a+'+'+b))
+print('''SHIP NAME      CLASS          DEPLOYMENT IN SERVICE
+N2 Bomber      Heavy Fighter  Limited    21        
+J-Type 327     Light Combat   Unlimited  1         
+NX Cruiser     Medium Fighter Limited    18        
+N1 Starfighter Medium Fighter Unlimited  25        
+Royal Cruiser  Light Combat   Limited    4         
+''')
 
 
 # print(b)
