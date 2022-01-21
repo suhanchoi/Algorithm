9836e85396767016e069be5f69b05d622816b3fc
suhan <suhan9603@naver.com>
2022-01-22 00:51:53 +0900

    0122 update

--- test.py
+++ test.py
@@ -2,19 +2,9 @@
 # a,b = map(int,(sys.stdin.readline().split()))
 # a,b = sys.stdin.readline().split()
 # print(eval(a+'+'+b))
-print('''NFC West       W   L  T
------------------------
-Seattle        13  3  0
-San Francisco  12  4  0
-Arizona        10  6  0
-St. Louis      7   9  0
-
-NFC North      W   L  T
------------------------
-Green Bay      8   7  1
-Chicago        8   8  0
-Detroit        7   9  0
-Minnesota      5  10  1      
+print('''.  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/.    
 ''')
 
 
