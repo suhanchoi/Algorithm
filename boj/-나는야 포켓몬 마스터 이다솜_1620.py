bf72d2523c098741458234cf6ea1c968b1248031
suhan <suhan9603@naver.com>
2022-01-23 22:24:20 +0900

    0123 update

--- boj/-나는야 포켓몬 마스터 이다솜_1620.py
+++ boj/-나는야 포켓몬 마스터 이다솜_1620.py
@@ -6,18 +6,20 @@ from sys import stdin
 #             return k
 
 mydict = {}
+idxdict = {} # 시간 초과 해결을 위한 역 참조 (값에서 -> 키 참조)
 
 total_num , num = map(int, stdin.readline().split())
 
 for i in range(total_num):
-    mydict[i+1]= stdin.readline().strip()
+    inputstring = stdin.readline().strip()
+    mydict[i+1]= inputstring
+    idxdict[inputstring] = i+1
 
-vlst = list(mydict.values())
+# vlst = list(mydict.values())
 
 for i in range(num):
     t = stdin.readline().strip()
-    if t.isalpha():
-        # print(get_k(t))
-        print(int(vlst.index(t))+1)
+    if t.isalpha(): # 이름이 들어가면
+        print(int(idxdict.get(t)))
         continue
-    print(mydict.get(int(t)))
+    print(mydict.get(int(t))) # 숫자가 들어가면
