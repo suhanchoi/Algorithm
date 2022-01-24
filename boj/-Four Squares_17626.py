c903a15e965f2fab0218a562e5bcf09362ef3471
suhan <suhan9603@naver.com>
2022-01-25 00:13:24 +0900

    0125 update

--- boj/-Four Squares_17626.py
+++ boj/-Four Squares_17626.py
@@ -2,11 +2,6 @@ from sys import stdin
 
 n = int(stdin.readline())
 
-for i in range(n):
-    url, pw = stdin.readline().split()
-    dct[url] = pw
 
-for i in range(m):
-    print(dct.get(stdin.readline().strip()))
 
 
