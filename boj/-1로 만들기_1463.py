fce6c8f431c8d9470d6bc1eaec9a3b1c16b81582
wajabu <suhan9603@naver.com>
2022-01-30 00:59:51 +0900

    0130 update

--- boj/-1로 만들기_1463.py
+++ boj/-1로 만들기_1463.py
@@ -6,7 +6,7 @@ n = int(stdin.readline())
 
 result = [0] * (n+1)
 
-for i in range(2,n+1) # 0:1. 1:1
+for i in range(2,n+1): # 0:1. 1:1
     if i % 6 == 0:
         result[i] = min(result[i//3]+1,result[i//2]+1, result[i-1]+1)
     elif i % 3 == 0:
