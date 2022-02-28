7846fc364f170159ea3460ee74c04a503c546ac5
suhan <suhan9603@naver.com>
2022-03-01 01:54:44 +0900

    0301 update

---
+++ boj/class 3+/1,2,3 더하기_9095.py
@@ -0,0 +1,32 @@
+from sys import stdin
+
+
+# dp, 재귀식
+
+def check(n):
+    global result
+
+    if n < 4:
+        print(result[n])
+        return
+
+    i = len(result)
+
+    if i-1 < n:
+        result.append(result[i - 1] + result[i - 2] + result[i - 3])
+        check(n)
+        return
+
+    if len(result)-1 >= n:
+        print(result[n])
+        return
+
+
+n = int(stdin.readline())
+
+result = [0, 1, 2, 4]
+
+lst = []
+
+for _ in range(n):
+    check(int(stdin.readline().strip()))
