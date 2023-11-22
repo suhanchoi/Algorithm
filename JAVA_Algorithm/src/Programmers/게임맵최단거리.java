9f671234f662e4e04c3938571cbe46ef2bdb11e7
suhan <suhan9603@naver.com>
2023-11-23 03:00:50 +0900

    231122 update

--- JAVA_Algorithm/src/Programmers/게임맵최단거리.java
+++ JAVA_Algorithm/src/Programmers/게임맵최단거리.java
@@ -28,7 +28,10 @@ class 게임맵최단거리1 {
         // 가장 빠른 최단거리를 출력
         // 도착할 수 없다면 return -1
         
-        bfs(int[][] maps);
+        // -> 시간초과로인해 다른 방법으로 풀어야함
+        // visited[][] 추가?
+        
+        bfs(maps);
         
         // n,m 의 값 확인
         if(cnt[maps.length -1][maps[0].length -1] == 0) {
@@ -52,36 +55,52 @@ class 게임맵최단거리1 {
     	
     	q.add(new int[] {0,0}); // 시작 위치 지정
     	
+    	int[][]	visited = new int[maps.length][maps[0].length];
+    	
+    	visited[0][0] = 1; // 방문처리
+    	
     	while(!q.isEmpty()) {
     		
     		int[] now = q.poll();
+    		
+    		// 방문 처리를 여기서 하면 시간이 오래걸림...
+    		
     		int x = now[0];
     		int y = now[1];
     		
+    		if(x == maps.length -1  && y == maps[0].length -1)
+    			break;
+    		
+    		int newX = 0, newY = 0;
+    		
 	    	// 상 하 좌 우
-	    	int[] dx = {-1, 1, 0, 0}; // 행  
-	    	int[] dy = {0, 0, -1, 1}; // 열
+	    	int[] dx = {-1, 1, 0, 0}; // 행  maps.length
+	    	int[] dy = {0, 0, -1, 1}; // 열  maps[0].length
 	    	
-	    	for(int i = 0; i < 4; i++) {
-	    		x = x + dx[i];
-	    		y = y + dy[i];
-	    	}
+	    	for(int i = 0; i < 4; i++) { // 상하좌우로 1칸씩 이동
+	    		newX = x + dx[i];
+	    		newY = y + dy[i];
+	    		
+		    	// 벽을 넘는지 아닌지
+		    	// 이동하려는 칸이 벽인지 확인
+		    	// 벽이 아니라면 경로에 추가 -> q.add(위치)
+	    		
+		    	if(newX >= 0 && newX <= maps.length -1 && newY >= 0 
+		    			&& newY <= maps[0].length -1) {
+		    		// map 안에 있는지 확인 후
+		    		
+		    		if(maps[newX][newY] == 1 && visited[newX][newY] != 1) { // 벽이 아니라 갈 수 있는 길이라면,
+		        		visited[newX][newY] = 1; // 방문처리
+		    			q.add(new int[] {newX, newY}); // 경로에 추가
+		    			cnt[newX][newY] = cnt[x][y] + 1; // 최단거리 업데이트
+		    		}
+		    	} // if 끝
+	    	} // for 끝
 	    	
-	    	// 벽을 넘는지 아닌지
-	    	// 이동하려는 칸이 벽인지 확인
-	    	// cnt == 0 즉 들리지 않은 곳인 지 확인 후
-	    	// 벽이 아니라면 경로에 추가 -> q.add(위치)
-	    	
-	    	// cnt ++;
 	    	// 이동한 타일은 벽 처리
-	    	
-    	
+	    	maps[x][y] = 0; // 방문처리와 같은 개념
     	}
-    	
-    	
     };
-    
-    
 }
 
 
@@ -107,11 +126,4 @@ class 게임맵최단거리1 {
 
 
 
-
-
-
-
-
-
-
-
+ 
--- JAVA_Algorithm/src/Programmers/입국심사.java
+++ JAVA_Algorithm/src/Programmers/입국심사.java
@@ -16,17 +16,42 @@ class 입국심사 { // https://school.programmers.co.kr/learn/courses/30/lessons/43
 }
 
 class 입국심사1 {
-	 
 	public static long solution(int n, int[] times) {
         long answer = 0;
         
+        // 심사받는 시간을 최소화
+        // n 기다리는 사람 수
+        // times은 각 심사관이 한명을 심사하는데 걸리는 시간 배열
+        // 비었더라도 빨리끝나는곳에 가서 심사 받기가 가능 
+        Arrays.sort(times);
         
-        
-        
+        // 시간을 기준으로 max min 이분탐색 확인
+        long min = 0, max = (long)n * (long)(times[times.length -1]) , mid;
+
+        while(min <= max) { // 탈출하게 된다면, 그때의 mid가 정답
+        	
+        	int sum = 0; // 심사한 인원 -> n이 되면 종료
+        	
+        	mid = (min + max) / 2; // mid 걸린 시간
+        	
+        	for(int i = 0; i < times.length; i++) {
+        		sum += mid / times[i]; // 시간을 통해 심사한 사람들
+        		// 시간의 최솟값을 걸리는 시간으로 나눠서
+        		// 사람명 수를 통해 이분탐색 
+        	}
+        	
+        	if (sum < n) { // 검사를 다 받지 못했다. 즉 시간이 부족함
+        		min = mid + 1; // 오른쪽 구간 탐색
+        		
+        	} else { // 검사를 다 하긴 했지만 더 최적이 있을 수 있다.
+        		// sum == n 경우는 딱 맞아 떨어지지 않을 수 있기 때문에 else로 처리
+        		max = mid - 1; // 왼쪽 구간 탐색
+        		answer = mid;
+        	}
+        }
         return answer;
     }
 }
-
 
 
 
